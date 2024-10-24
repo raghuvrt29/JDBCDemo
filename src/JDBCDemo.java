f42607993e88a79a2d2ed49a025cad70f6c217b8
raghuvrt29 <raghu.vrt29@gmail.com>
2024-10-24 23:28:34 +0530

    Executing insert and update query

--- src/JDBCDemo.java
+++ src/JDBCDemo.java
@@ -17,20 +17,29 @@ public class JDBCDemo {
             String url = "jdbc:postgresql://localhost:5432/JDBCDemo";
             String username = "postgres";
             String password = "0000";
-            String sql = "select * from students;";
 
             Connection conn = DriverManager.getConnection(url,username,password);
-
             System.out.println("Connection Established");
-
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            while(rs.next()){
-                System.out.print(rs.getInt(1) + " - ");
-                System.out.print(rs.getString(2) + " - ");
-                System.out.println(rs.getInt(3));
 
-            }
+//            // Executing Select Query
+//            String sql = "select * from students;";
+//            ResultSet rs = st.executeQuery(sql);
+//            while(rs.next()) {
+//                System.out.print(rs.getInt(1) + " - ");
+//                System.out.print(rs.getString(2) + " - ");
+//                System.out.println(rs.getInt(3));
+//            }
+
+//            // Executing Insert Query
+//            String sql = "insert into students values(5,'Srinivas Chavali',23)";
+//            boolean status = st.execute(sql);
+//            System.out.println("Status: " + status);
+
+            //Executing Update Query
+            String sql = "update students set name='Srinivas Chodisetti' where id=5";
+            int num = st.executeUpdate(sql);
+            System.out.println("Rows effected: "+num);
 
             conn.close();
             System.out.println("Connection Closed");
