b9c2618b0772e2d9b376ae984ae37490b2a2b1e2
raghuvrt29 <raghu.vrt29@gmail.com>
2024-10-24 23:07:52 +0530

    Executing select query

--- src/JDBCDemo.java
+++ src/JDBCDemo.java
@@ -14,14 +14,26 @@ public class JDBCDemo {
 
         try {
 
-            String url="jdbc:postgresql://localhost:5432/JDBCDemo";
-            String username="postgres";
-            String password="0000";
+            String url = "jdbc:postgresql://localhost:5432/JDBCDemo";
+            String username = "postgres";
+            String password = "0000";
+            String sql = "select * from students;";
 
-            Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection(url,username,password);
 
             System.out.println("Connection Established");
+
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+                System.out.print(rs.getInt(1) + " - ");
+                System.out.print(rs.getString(2) + " - ");
+                System.out.println(rs.getInt(3));
+
+            }
+
+            conn.close();
+            System.out.println("Connection Closed");
 
         } catch(Exception e){
             System.out.println(e.getMessage());
